bcf567e9ed87956013e84fc58051bc6289ed2186
Incognitux <naanx2006@gmail.com>
2025-05-09 18:58:05 +0530

    add functionality to breadcrums

--- src/Components/Breadcrum/Breadcrum.jsx
+++ src/Components/Breadcrum/Breadcrum.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './Breadcrum.css'
+import { Link } from 'react-router-dom'
 import arrow_icon from '../Assests/breadcrum_arrow.png'
 
 function Breadcrum(props) {
     const {product} = props;
-  return (
-    <div className='breadcrum'>
-       HOME <img src={arrow_icon} alt='img'></img> SHOP <img src={arrow_icon}  alt='img'></img> {product.category} <img src={arrow_icon}  alt='img'></img>  {product.name}
-    </div>
-  )
+    if (!product || !product.category || !product.name) {
+        return <div>Loading breadcrumb...</div>;
+    }
+
+    const categoryPath = `/${product.category.toLowerCase()}`;
+
+    return (
+        <div className='breadcrum'>
+            <Link to='/'>SHOP</Link> <img src={arrow_icon} alt='>' />
+            <Link to={categoryPath}>{product.category.toUpperCase()}</Link> <img src={arrow_icon} alt='>' />
+            {product.name}
+        </div>
+    );
 }
 
 export default Breadcrum
